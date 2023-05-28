415865fdd1df43c1779fe90390321921a6fc9e49
Shin <sin37minduke@naver.com>
2023-05-28 11:31:01 +0900

    Need CSV Parsing.

--- Assets/Script/Dialogue/CSVParser.cs
+++ Assets/Script/Dialogue/CSVParser.cs
@@ -1,37 +1,38 @@
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
 public class CSVParser : MonoBehaviour
 {
+    public static CSVParser instance;
+
     [Header("제목 행 인덱스")]
     public int titleLine;
 
     [Header("파일명")]
     public string fileName;
 
-    private void Start()
+    private void Awake()
     {
-        ParseCSVFile(fileName);
+        if (!instance) instance = this;
     }
 
-    public void ParseCSVFile(string filePath)
+    public List<Dictionary<string, string>> ParseCSVFile()
     {
-        if (File.Exists(filePath) == false)
+        if (File.Exists(fileName) == false)
         {
             Debug.Log("파일 이름 혹은 경로를 확인하세요");
-            return;
+            return null;
         }
 
         // CSV 파일 읽기
-        string[] csvLines = File.ReadAllLines(filePath);
-
-        Debug.Log(csvLines.Length);
+        string[] csvLines = File.ReadAllLines(fileName);
 
         if (csvLines.Length < 4)
         {
             Debug.LogError("CSV 파일이 유효하지 않습니다.");
-            return;
+            return null;
         }
 
         // 컬럼명 추출
@@ -41,6 +42,15 @@ public class CSVParser : MonoBehaviour
         List<Dictionary<string, string>> parsedData = new List<Dictionary<string, string>>();
         for (int i = titleLine; i < csvLines.Length; i++)
         {
+            Debug.Log(csvLines[i]);
+            // 따옴표로 감싸진 텍스트인 경우 따옴표 제거하고 이스케이프된 따옴표 처리
+            if (csvLines[i].StartsWith("\"") && csvLines[i].EndsWith("\""))
+            {
+                csvLines[i] = csvLines[i].Substring(1, csvLines[i].Length - 2); // 따옴표 제거
+                csvLines[i] = csvLines[i].Replace("\"\"", "\""); // 이스케이프된 따옴표 처리
+            }
+            Debug.Log(csvLines[i]);
+
             string[] dataValues = csvLines[i].Split(',');
             Dictionary<string, string> rowData = new Dictionary<string, string>();
 
@@ -49,20 +59,21 @@ public class CSVParser : MonoBehaviour
             {
                 string columnName = columnNames[j];
                 string dataValue = (j < dataValues.Length) ? dataValues[j] : string.Empty;
+
                 rowData[columnName] = dataValue;
             }
 
             parsedData.Add(rowData);
         }
 
-        // 파싱된 데이터 사용 예시
-        foreach (var row in parsedData)
-        {
-            Debug.Log("Row Data:");
-            foreach (var kvp in row)
-            {
-                Debug.Log(kvp.Key + ": " + kvp.Value);
-            }
-        }
+        return parsedData;
+    }
+
+    public List<Dictionary<string, string>> ParseCSVFile_EventID(string eventID)
+    {
+        List<Dictionary<string, string>> parsedData = ParseCSVFile();
+        List<Dictionary<string, string>> returnData = parsedData.Where(row => row.ContainsKey("Event ID") && row["Event ID"] == eventID).ToList();
+
+        return returnData;
     }
 }
--- Assets/Script/Dialogue/SubDialogueData.cs
+++ Assets/Script/Dialogue/SubDialogueData.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class SubDialogueData : MonoBehaviour
 {
-    
+    public string eventId;
+    public List<Dictionary<string, string>> scriptData;
+
+    public TMPro.TMP_InputField inputEventID;
+    public TMPro.TextMeshProUGUI korText;
+    public TMPro.TextMeshProUGUI type;
+
+    private int currentTextIndex = 0;
+
+    private void Start()
+    {
+        scriptData = CSVParser.instance.ParseCSVFile_EventID("");
+        OnCurrentText();
+    }
+
+    public void OnSetEventID()
+    {
+        scriptData = CSVParser.instance.ParseCSVFile_EventID(inputEventID.text);
+        currentTextIndex = 0;
+        OnCurrentText();
+    }
+
+    public void OnCurrentText()
+    {
+        korText.text = scriptData[currentTextIndex]["Kor Text"];
+        type.text = scriptData[currentTextIndex]["Type"];
+    }
+
+    public void OnNextText()
+    {
+        currentTextIndex++;
+        korText.text = scriptData[currentTextIndex]["Kor Text"];
+        type.text = scriptData[currentTextIndex]["Type"];
+    }
 }
