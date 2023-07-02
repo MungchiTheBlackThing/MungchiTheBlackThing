0d7812857efd28ae2344e9ee697fa1376641e049
songsuyoung <tndud3999@naver.com>
2023-07-02 23:25:54 +0900

    [Bugfix] cnt overflow문제 해결

--- Assets/03.Script/ChatClickManager.cs
+++ Assets/03.Script/ChatClickManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 /*
 1. 현재 모든 자식을 불러온다.
 2. 클릭한 자식의 인덱스를 확인한다.
@@ -11,28 +12,37 @@ using UnityEngine.EventSystems;
 public class ChatClickManager : MonoBehaviour
 {
 
+    /*
+    클릭할 때 AreaScript에게 다음 GameObject 전달
+    AreaScript는 누를시 전달받은 GameObject 생성, 자기자신 비활성화 
+    */
+
     [SerializeField]
-    GameObject[] dreamNPC;
-
-    public void OnClick(){
-        //클릭된 오브젝트의 클릭 비활성화
-        GameObject clickObject =EventSystem.current.currentSelectedGameObject;
-        Transform parentObject=clickObject.transform.parent;
-
+    protected GameObject[] dreamNPC;
+    public int currIdx=0;
+    //임시 대사 저장
+    protected List<string> txt= new List<string>(){
+        "~♫♮♭♩♫&?","숨겨 놓고 아껴 먹던 건데… 잃어버렸을 리가.","그것, 첼로 경이 뭉치에 꽂아 보냄.\n뭉치도 간식 필요하다 했음.",
+        "!!♫♮♭#)&!!","비밀이라고 말한 적은 없지 않았음? 기억 안 남.","아, 그걸 왜 걔를 줘! 걔는 먹지도 못해!"
+    };
+    protected int [] idx=new int[6]{1,2,0,1,0,2};
+    void Start(){
+        currIdx=0;
+    }
+    public void RunScript(){
+        if(currIdx>=idx.Length)
+            return;
+        
+        Debug.Log(currIdx);
+        Transform parentObject=this.gameObject.transform;
         if(parentObject.childCount>=3){
             Destroy(parentObject.GetChild(0).gameObject);
-            //클릭되었을때 부모의 자식 개수가 3개를 넘어서면 가장 최상위 자식 삭제
-            //Destroy(clickObject.transform.parent.GetChild(1));
+                //클릭되었을때 부모의 자식 개수가 3개를 넘어서면 가장 최상위 자식 삭제
+                //Destroy(clickObject.transform.parent.GetChild(1));
         }
-        clickObject.GetComponent<Button>().interactable=false;
-        //현재 다이어로그를 불러오지 않은 상태이기에, 랜덤으로 불러오도록 완성한다.
-
-        //랜덤 호출 부분 -> dreamNPC의 순서를 저장하고 호출하는 방법으로 변경할 예정.
-        int randIdx=Random.Range(0,dreamNPC.Length);
-        GameObject obj=Instantiate(dreamNPC[randIdx]) as GameObject; //gameObject 생성
-        obj.name=obj.name.Substring(0,obj.name.IndexOf('('));
-        obj.transform.SetParent(parentObject,false); //현재 클릭된 오브젝트의 부모 위로 설정해주기.
-
-
+        GameObject newObject=Instantiate(dreamNPC[idx[currIdx]]) as GameObject; //gameObject 생성
+        newObject.name=newObject.name.Substring(0,newObject.name.IndexOf('('));
+        newObject.transform.GetComponent<AreaScript>().CharacterText.text=txt[currIdx-1];
+        newObject.transform.SetParent(parentObject,false); //현재 클릭된 오브젝트의 부모 위로 설정해주기.
     }
 }
--- Assets/03.Script/UI/AreaScript.cs
+++ Assets/03.Script/UI/AreaScript.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class AreaScript : MonoBehaviour
+public class AreaScript : MonoBehaviour,IPointerDownHandler
 {
 
     [SerializeField]
     RectTransform AreaRect, TextRect, CharacterRect;
     [SerializeField]
-    TMP_Text CharacterText;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public TMP_Text CharacterText;
+    ChatClickManager manager;
+    bool isFirst=false;
+    private void OnEnable() {
+        manager=GameObject.Find("Content").GetComponent<ChatClickManager>();
+        if(manager)
+            manager.currIdx+=1;    
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /*눌렀어. 누르면 ChatClickManager을 호출해서 다음 스크립트 호출*/
+    public void OnPointerDown(PointerEventData eventData){
+        if(!isFirst){
+            manager.RunScript();
+            isFirst=true;
+        }
     }
 }
