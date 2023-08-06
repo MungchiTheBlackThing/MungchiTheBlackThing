e5c2cc2def5624aac747c1d3cd0619df131a04d6
songsuyoung <tndud3999@naver.com>
2023-08-06 11:11:23 +0900

    [Develop] 달나라 송신기 애니메이션 추가

--- Assets/03.Script/MoonRadio/MainNightSystem.cs
+++ Assets/03.Script/MoonRadio/MainNightSystem.cs
@@ -8,6 +8,6 @@ public class MainNightSystem : MonoBehaviour
     [SerializeField]
     GameObject moon_main;
     public void InstMoonSystem(){
-        Instantiate(moon_main,this.transform.parent);
+        Instantiate(moon_main,this.transform.parent.transform.parent);
     }
 }
