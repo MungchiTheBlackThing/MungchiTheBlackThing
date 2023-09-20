9d75ed8f6249ab5132b774665202bb06493258bc
Invisible1nk <simpson7257@gmail.com>
2023-09-20 12:49:24 +0900

    [Develop] Android 확인

--- Docker/server.js
+++ Docker/server.js
@@ -1,17 +1,17 @@
 'use strict';
- 
+
 const express = require('express');
-require('dotenv').config();
+
 // 상수
-const PORT = process.env.PORT
-const HOST = process.env.HOST
- 
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
 // 앱
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
- 
+
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
