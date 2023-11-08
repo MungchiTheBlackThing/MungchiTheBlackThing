7f1e8c94e0e4bc0b8ee0216ce39ab25c51c663ea
songsuyoung <tndud3999@naver.com>
2023-11-08 15:32:23 +0900

    ScrollRect 오류 수정 후 적용

--- Assets/03.Script/DefaultController.cs
+++ Assets/03.Script/DefaultController.cs
@@ -14,8 +14,7 @@ public class DefaultController : MonoBehaviour
     [SerializeField]
     PlayerInfo player; //이거 플레이어 말고 GameManager부분으로 이동해야할거같음.
     Canvas canvas;
-    GraphicRaycaster raycaster;
-    PointerEventData point;
+
     public void Start()
     {
         isClose=false;
@@ -23,8 +22,6 @@ public class DefaultController : MonoBehaviour
         RectTransform rectTransform = this.gameObject.GetComponent<RectTransform>();
         DefaultPos = this.transform.position;
         canvas=this.GetComponent<Canvas>();
-        raycaster=canvas.GetComponent<GraphicRaycaster>();
-        point = new PointerEventData(null);
     }
     public void Update()
     {
@@ -37,64 +34,23 @@ public class DefaultController : MonoBehaviour
         {
             scrollRect.horizontal = true;
         }
-
-        /*
-        그래픽에 레이캐스트를 쏜다.
-         레이어 fixAnimator에 해당하는 오브젝트가 있는가를 확인한다.
-         있으면? isClick을 실행한다.
-         */
-
-        GameObject selectedObj=checkFirstRaycasterObj();
-        if(selectedObj){
-            switch(selectedObj.name){
-                case "fix_door_open": case "fix_door_close" :
-                OpenDoor(selectedObj);
-                break;
-                case "fix_binoocular":
-                setBinocular(selectedObj);
-                break;
-                case "fix_moonradio":
-                //밤이라면 문라디오 실행
-                if(player.GetCurrTime()=="night"){
-                    InstMoonSystem(selectedObj);
-                    this.gameObject.SetActive(false);
-                    //라디오를 누르면 잠시 꺼놓는다.
-                    //라디오 종료시 다시 켜놓는다.
-                }
-                break;
-                case "phase_letter":
-                    selectedObj.GetComponent<NoteClick>().setNote();
-                break;
-                default:
-                    isClick(selectedObj);
-                    break;
-            }
-        }
     }
-    public void InstMoonSystem(GameObject selectedObj){
+    public void InstMoonSystem(){
+        if(player.GetCurrTime()!="night")
+            return ;
         Instantiate(moon_main,this.transform.parent);
     }
-    GameObject checkFirstRaycasterObj(){
-        point.position=Input.mousePosition;
-        List<RaycastResult> results = new List<RaycastResult>();
-        if(Input.GetMouseButtonDown(0)){
-            raycaster.Raycast(point,results);
-        }
 
-        if(results.Count==0)
-            return null;
-
-        return results[0].gameObject;
-    }
     //Animating 
-    public void isClick(GameObject selected){
+    public void isClick(){
+        GameObject selected=EventSystem.current.currentSelectedGameObject;
         Animator ani=selected.GetComponent<Animator>();
         if(ani!=null)
             ani.SetTrigger("isTouch");
     }
 
-    public void setBinocular(GameObject parent_Bino){
-        //GameObject parent_Bino=EventSystem.current.currentSelectedGameObject;
+    public void setBinocular(){
+        GameObject parent_Bino=EventSystem.current.currentSelectedGameObject;
         if(parent_Bino.transform.GetChild(0).gameObject.activeSelf){ 
             Instantiate(Resources.Load("Bino_"+player.GetCurrDay().ToString())); //Bino이름 동적으로 바꿔야함.
             this.transform.parent.gameObject.SetActive(false);
@@ -102,8 +58,9 @@ public class DefaultController : MonoBehaviour
         }
     }
 
-    public void OpenDoor(GameObject child){
-        child.transform.parent.GetComponent<Animator>().SetBool("isClose",isClose);
+    public void OpenDoor(){
+        GameObject selected=EventSystem.current.currentSelectedGameObject;
+        selected.transform.parent.GetComponent<Animator>().SetBool("isClose",isClose);
         isClose=!isClose;
     }
 }
--- Assets/03.Script/GameManager.cs
+++ Assets/03.Script/GameManager.cs
@@ -8,27 +8,18 @@ public class GameManager : MonoBehaviour
     const int setHeight=1920;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Camera.main.transform.position=new Vector3(0,0,-10f);
-    }
-
     void setResolution(){
+        Camera.main.transform.position=new Vector3(0,0,-10f);
         //현 기기의 너비와 높이
         int deviceWidth=Screen.width;
         int deviceHeight=Screen.height;
         //기기의 해상도 비율을 비로 조절
 
         Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
-         if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
-        {
-            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
-            //Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용 -> 우리는 캔버스만 반응형으로 구현할 예정. 
-        }
-        else // 게임의 해상도 비가 더 큰 경우
+        if ((float)setWidth / setHeight >= (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
         {
             float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
-            //Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
         }
 
     }
