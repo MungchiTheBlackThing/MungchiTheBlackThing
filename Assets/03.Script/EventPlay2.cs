d300331dcf5503417f34e100d3915fd7320baa5f
songsuyoung <tndud3999@naver.com>
2023-12-06 16:18:15 +0900

    merge

--- Assets/03.Script/EventPlay2.cs
+++ Assets/03.Script/EventPlay2.cs
@@ -5,13 +5,12 @@ using UnityEngine.EventSystems;
 
 public class EventPlay2 : MonoBehaviour,IPointerDownHandler
 {
-    public GameObject Mungchi;
-    public GameObject Diary;
+        public GameObject Diary;
 
     // Start is called before the first frame update
     void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
     {
-        Animator animator = Mungchi.GetComponent<Animator>();
+        Animator animator = this.transform.parent.GetComponent<Animator>();
         Diary.SetActive(true);
         animator.SetTrigger("read");
         Debug.Log("read");
