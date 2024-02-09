d7e6585aaa23005ed3008a3d00668340d5a494f3
songsuyoung <tndud3999@naver.com>
2024-02-09 16:54:59 +0900

    [Develop] size 디졸브 현상 추가

--- Assets/03.Script/Contents/Cursor.cs
+++ Assets/03.Script/Contents/Cursor.cs
@@ -6,6 +6,7 @@ using UnityEngine.EventSystems;
 public class Cursor : MonoBehaviour
 {
     public bool isrelease = false;
+    public bool isSuccess = false;
     public GameObject target;
     void Start(){
         isrelease = false;
@@ -21,26 +22,30 @@ public class Cursor : MonoBehaviour
     }
     private void Update() 
     {
-        if (Input.GetMouseButton(0))
+        if(isSuccess == false)
         {
-            ObjectMove();
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            List<RaycastResult> results = new();
-            //마우스 클릭한 좌표값 가져오기
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //해당 좌표에 있는 오브젝트 찾기
-            RaycastHit2D[] hit = Physics2D.RaycastAll(pos, Vector2.zero, 0f);
+            if (Input.GetMouseButton(0))
+            {
+                ObjectMove();
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                List<RaycastResult> results = new();
+                        //마우스 클릭한 좌표값 가져오기
+                Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                        //해당 좌표에 있는 오브젝트 찾기
+                RaycastHit2D[] hit = Physics2D.RaycastAll(pos, Vector2.zero, 0f);
 
-            for (int i = 0; i < hit.Length; i++)
-                if (hit[i].collider.tag == "Mungchi")
-                {
-                    target.GetComponent<MungchiClick>().OnMouseDown();
-                    Debug.Log("뭉치 찾음");
-                    break;
-                }
+                for (int i = 0; i < hit.Length; i++)
+                    if (hit[i].collider.tag == "Mungchi")
+                    {
+                        target.GetComponent<MungchiClick>().OnMouseDown();
+                        isSuccess=true;
+                        break;
+                    }
+            }
         }
+        
     }
 
     private void ObjectMove()
