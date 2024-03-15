a66b9bb8c46597c87c2b24fc611116347d12b174
Invisible1nk <simpson7257@gmail.com>
2024-03-15 13:53:36 +0900

    merge
    .

--- Assets/03.Script/SubMungchi.cs
+++ Assets/03.Script/SubMungchi.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class SubMungchi : MonoBehaviour
 {
     public GameObject triggericon;
-    public SubDialEvent subdial;
+    private SubDialEvent subdial;
     // Start is called before the first frame update
     void Start()
     {
