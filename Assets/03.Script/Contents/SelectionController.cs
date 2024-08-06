179cd6c6a9192ed0103947785bbf69d8726beef9
Invisible1nk <simpson7257@gmail.com>
2024-08-06 10:34:19 +0900

    체크 박스 하나만 가능하게 수정
    .

--- Assets/03.Script/Contents/SelectionController.cs
+++ Assets/03.Script/Contents/SelectionController.cs
@@ -1,22 +1,44 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class SelectionController : MonoBehaviour
 {
-    //isFour 변수에 따라 선택지 갯수가 달라진다.
+    // isFour 변수에 따라 선택지 갯수가 달라진다.
     [SerializeField]
     bool isFour;
-    //선택지는 나중에 저장한다.
-    public void Choose(){
-        GameObject option=UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
 
-        if(option.transform.GetChild(0).gameObject.activeSelf){
-            option.transform.GetChild(0).gameObject.SetActive(false);
-        }else{
-            option.transform.GetChild(0).gameObject.SetActive(true);
+    // 선택지를 저장할 리스트
+    [SerializeField]
+    List<GameObject> options;
+
+    // 현재 선택된 체크박스의 인덱스를 저장할 변수
+    public int selectedIndex = -1;
+
+    // 선택지를 선택하는 메서드
+    public void Choose()
+    {
+        GameObject option = EventSystem.current.currentSelectedGameObject;
+
+        if (option == null)
+        {
+            return;
+        }
+
+        // 선택된 옵션의 인덱스를 찾기
+        int newSelectedIndex = options.IndexOf(option);
+
+        // 모든 선택지를 비활성화
+        for (int i = 0; i < options.Count; i++)
+        {
+            options[i].transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+        // 새로운 선택된 체크박스를 활성화
+        if (newSelectedIndex != -1)
+        {
+            options[newSelectedIndex].transform.GetChild(0).gameObject.SetActive(true);
+            selectedIndex = newSelectedIndex;
         }
     }
-
 }
